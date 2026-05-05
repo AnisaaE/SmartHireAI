4aa04c55f184cb5f3140a9881d9a5dd52a2c2dc1
Anisa <anisaemin97@gmail.com>
2026-05-05 21:47:39 +0300

    Initialize AI analysis service

---
+++ ai_analysis/ai_analysis/src/main/java/com/smart_hire/ai_analysis/AiAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.smart_hire.ai_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAnalysisApplication.class, args);
+	}
+
+}
---
+++ ai_analysis/ai_analysis/src/test/java/com/smart_hire/ai_analysis/AiAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_hire.ai_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
