aa1330f97d19e94e1c758646328671d550cd8bc3
nalankaraa <nalankaraa3@gmail.com>
2026-05-05 21:35:45 +0300

    Initialize document service

---
+++ document/src/test/java/com/smart_hire/document/DocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_hire.document;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
