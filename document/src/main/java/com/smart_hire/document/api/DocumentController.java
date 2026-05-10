02d302391f56a3190db10723b4f8dbde44d6689b
Anisa <anisaemin97@gmail.com>
2026-05-10 20:29:00 +0300

    feat: [GREEN] implement upload document endpoint

---
+++ document/src/main/java/com/smart_hire/document/api/DocumentController.java
@@ -0,0 +1,18 @@
+package com.smart_hire.document.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/documents")
+class DocumentController {
+
+    @PostMapping("/upload")
+    @ResponseStatus(HttpStatus.CREATED)
+    void uploadDocument(String ownerId, String type, String title, MultipartFile file) {
+    }
+}
