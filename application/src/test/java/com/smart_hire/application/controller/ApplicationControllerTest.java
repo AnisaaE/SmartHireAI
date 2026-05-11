f76e385146a5c368bc0e51aee6e2a30512c0a1ed
nalankaraa <nalankaraa3@gmail.com>
2026-05-11 21:00:25 +0300

    test: [RED] create application service scaffold and fail apply endpoint test

---
+++ application/src/test/java/com/smart_hire/application/controller/ApplicationControllerTest.java
@@ -0,0 +1,41 @@
+package com.smart_hire.application.controller;
+
+import com.smart_hire.application.dto.CreateApplicationRequest;
+import com.smart_hire.application.service.ApplicationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ApplicationController.class)
+class ApplicationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ApplicationService applicationService;
+
+    @Test
+    void shouldReturnCreatedWhenApplyPayloadIsValid() throws Exception {
+        mockMvc.perform(post("/api/applications")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                  "jobId": 12,
+                                  "candidateId": 34,
+                                  "cvDocumentId": "cv-doc-101"
+                                }
+                                """))
+                .andExpect(status().isCreated());
+
+        verify(applicationService).apply(any(CreateApplicationRequest.class));
+    }
+}
